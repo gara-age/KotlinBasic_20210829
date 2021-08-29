83b71e1ef5795992656921443bfad3dee9c1f788
최민서 <minseo00515@naver.com>
2021-08-29 10:44:48 +0900

    토스트 띄우기 예시

--- app/src/main/java/com/nepplus/kotlinbasic_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20210829/MainActivity.kt
@@ -3,13 +3,24 @@ package com.nepplus.kotlinbasic_20210829
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toastBtn.setOnClickListener {
+//            토스트 - "반갑습니다." 문장 띄우기
+
+            Toast.makeText(this, "반갑습니다.", Toast.LENGTH_SHORT).show()
+
+
+
+        }
 
         logBtn.setOnClickListener {
 //            이 중괄호 내부의 코드는, logBtn이 클릭될 때마다 실행됨
@@ -25,6 +36,8 @@ class MainActivity : AppCompatActivity() {
             Log.e("메인화면" , "두번째 로그 버튼 눌림")
 
         }
+
+
 
     }
 }

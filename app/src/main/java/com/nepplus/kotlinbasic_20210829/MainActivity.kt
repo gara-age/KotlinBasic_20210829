442115b1c07b748514c0920526766cf3db3cdc2a
최민서 <minseo00515@naver.com>
2021-08-29 10:39:53 +0900

    두번째 로그 버튼 추가

--- app/src/main/java/com/nepplus/kotlinbasic_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20210829/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,13 @@ class MainActivity : AppCompatActivity() {
 
              Log.d("메인화면" , "로그 버튼 눌림")
 
+
+        }
+
+        secondLogBtn.setOnClickListener {
+//            두번째 버튼이 눌리면 할일은 따로 지정
+
+            Log.e("메인화면" , "두번째 로그 버튼 눌림")
 
         }
 
